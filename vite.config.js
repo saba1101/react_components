c83ee20b4480253179ad48fd74b4dd77fca9f21b
saba1101 <sabakhara44@gmail.com>
2023-05-18 01:18:23 +0400

    ....

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    react(),
+    {
+      include: "**/*.jsx",
+    },
+    federation({
+      name: "masterComponents",
+      filename: "remoteEntry.js",
+      exposes: {
+        
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/global/_Variables.scss" as *;
+          @use "@/styles/global/_Mixins.scss" as *;
+        `
+      }
+    },
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+  },
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+})
