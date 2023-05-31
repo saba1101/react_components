393a27f78fafe0a08c4c130e02395017a15e6efe
saba2446 <sabakhara44@gmail.com>
2023-05-31 17:03:42 +0400

    ....

--- src/views/FormView/CheckboxView/CheckboxView.jsx
+++ src/views/FormView/CheckboxView/CheckboxView.jsx
@@ -1,6 +1,6 @@
 import '@/views/ViewsCommon/common.scss'
 import { useRef, useState } from 'react'
-import Checkbox from '@/components/Form/FormControls/CheckBox/CheckBox.jsx'
+import Checkbox from '@/components/Form/FormControls/Checkbox/Checkbox.jsx'
 import PropsDoc from '@/localComponents/PropsDoc.jsx'
 
 const CheckboxView = () => {
